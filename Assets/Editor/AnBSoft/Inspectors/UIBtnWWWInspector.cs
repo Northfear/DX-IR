ee31f4f268030da68d9852b2697f751c650f1ecf
Taras Rohozhynskyi <11291116+Northfear@users.noreply.github.com>
2024-09-15 13:36:08 +0300

    Imported editor scripts and assets for EZ GUI (v1.0798). Sorted most of the plugin files

---
+++ Assets/Editor/AnBSoft/Inspectors/UIBtnWWWInspector.cs
@@ -0,0 +1,17 @@
+//-----------------------------------------------------------------
+//  Copyright 2010 Brady Wright and Above and Beyond Software
+//	All rights reserved
+//-----------------------------------------------------------------
+
+
+using UnityEditor;
+using UnityEngine;
+using System.Collections;
+
+// Only compile if not using Unity iPhone
+#if !UNITY_IPHONE || (UNITY_3_0 || UNITY_3_1 || UNITY_3_2 || UNITY_3_3 || UNITY_3_4 || UNITY_3_5 || UNITY_3_6 || UNITY_3_7 || UNITY_3_8 || UNITY_3_9 || UNITY_4_0 || UNITY_4_1 || UNITY_4_2 || UNITY_4_3 || UNITY_4_4 || UNITY_4_5 || UNITY_4_6 || UNITY_4_7 || UNITY_4_8 || UNITY_4_9)
+[CustomEditor(typeof(UIBtnWWW))]
+#endif
+public class UIBtnWWWInspector : UICtlInspector
+{
+}
